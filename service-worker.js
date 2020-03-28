1db3f795e45506a978d62f8fd265483aa4ddb78f
Emupedia <admin@emupedia.net>
2020-03-28 16:20:18 +0200

    More work

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/diabloweb/precache-manifest.457140d01593ad0a7e968f24ae827c55.js"
+  "precache-manifest.457140d01593ad0a7e968f24ae827c55.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/diabloweb/index.html"), {
-  
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("index.html"), {
+
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
